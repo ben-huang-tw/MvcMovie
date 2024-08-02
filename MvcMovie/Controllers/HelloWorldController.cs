6bb320d0bb6dbe9c169232fa85e049542419b2af
Ben Huang <huangsb@gmail.com>
2024-08-02 15:19:03 +0800

    完成「新增控制器」
    * 教學網址: https://learn.microsoft.com/zh-tw/aspnet/mvc/overview/getting-started/introduction/getting-started

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index() {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        //public string Welcome() {
+        //    return "This is the Welcome action method...";
+        //}
+
+        // 
+        // GET: /HelloWorld/Welcome?name=Scott&numtimes=4
+        //public string Welcome(string name, int numTimes = 1) {
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        //}
+
+        // GET: /HelloWorld/Welcome/1?name=Scott
+        // GET: /HelloWorld/Welcome/Scott/3
+        public string Welcome(string name, int ID = 1) {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
